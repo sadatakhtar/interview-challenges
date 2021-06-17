401dc8e3fe38e6a7d31319aefee4f279d252c5e7
sadatakhtar <sadat0akhtar@gmail.com>
2021-06-17 12:38:22 +0100

    created excercises section one, still not complete

---
+++ css-challenge/src/components/excercises/Excercises.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Excercises.css'
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
+function Excercises() {
+  return (
+    <div className="excercises_container">
+      <div className="excercises_title">
+        <h2>Excercises</h2>
+        <div className="excercises_subheading_dots">
+          <FiberManualRecordIcon fontSize="small" />
+          <FiberManualRecordIcon fontSize="small" />
+        </div>
+      </div>
+      <div className="excercises_skills">
+        <div className="excersises_speaking">
+          <div className="excersices_speaking_logo">
+            <FavoriteIcon fontSize='large'/>
+          </div>
+          <div className="excercises_speaking_title">
+            <h3>Speaking Skills</h3>
+            <p>16 Excercises</p>
+          </div>
+        </div>
+        <div className="excersises_reading">
+          <div className="excersices_reading_logo"></div>
+          <div className="excersices_reading_title"></div>
+        </div>
+        <div className="excercises_listening">
+          <div className="excersices_listening_logo"></div>
+          <div className="excersices_listening_title"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Excercises
